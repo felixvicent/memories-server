a4b993a04a2a875b2eefdafb7467452074f852fe
Félix Vicente <felixvicent1306@gmail.com>
2023-05-21 14:54:20 -0300

    day 5

--- src/routes/memories.ts
+++ src/routes/memories.ts
@@ -21,6 +21,7 @@ export async function memoriesRoutes(app: FastifyInstance) {
       id: memory.id,
       coverUrl: memory.coverUrl,
       excerpt: memory.content.substring(0, 155).concat('...'),
+      createdAt: memory.createdAt,
     }))
   })
 
